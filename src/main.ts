0e847c03397fbda739979c0e7ffbe18aaddedf35
Jámbor Máté <jmate2007@gmail.com>
2026-09-14 10:36:56 +0200

    óra végei commit, esélyes hogy ki lesz törölve egy része, ha rossz

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
 import './style.css'
+import { ElectricRat} from './ElectricRat'
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const form = document.getElementById("form") as HTMLFormElement;
+  const nameInput = document.getElementById("name") as HTMLInputElement;
+
+  form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const ratties = [];
+    const name = nameInput.value
+    const atk = Math.floor(Math.random()*(20-10+1)+10) 
+    const hp =  Math.floor(Math.random()*(100-50+1)+50) 
+    ratties.push (new ElectricRat(name.toString(), atk, hp))
+    console.log(ratties)
+    form.reset()
+
+    const container = document.getElementById("cards")
+    const card = document.getElementById("div")
+    const ratName = document.createElement("p")
+    const ratHp = document.createElement("p")
+    const ratAtk = document.createElement("p")
+    ratName.appendChild(ratName)
+    ratHp.appendChild(ratHp)
+    
+
+
+
+
+  })
+})
